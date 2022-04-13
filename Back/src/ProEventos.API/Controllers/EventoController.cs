5657113afa09c28c1516fa63d8d1541cc86ea0ae
Gabriel Hakkinen <79407853+hakkGDC@users.noreply.github.com>
2022-04-13 10:49:23 -0300

    Initial commit

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using ProEventos.API.Data;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    
+    public class EventoController : ControllerBase
+    {
+       
+        private readonly DataContext _context;
+        public EventoController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _context.Eventos;
+        } 
+        [HttpGet("{id}")]
+
+        public Evento GetById(int id)
+        {
+            return _context.Eventos.FirstOrDefault(evento => evento.EventoId == id);
+        }            
+    
+        [HttpPost]
+            public string Post()
+            {
+                return "Post Example";
+            }
+        
+        [HttpPut ("{id}")]
+            public string Put(int id)
+            {
+                return $"Put Example with id: {id}";
+            }
+        
+        [HttpDelete ("{id}")]
+            public string Delete(int id)
+            {
+                return $"Delete Example deleting id: {id}";
+            }
+    }
+}
