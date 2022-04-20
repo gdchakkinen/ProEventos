e134359903243a617e8da6e5d6ed0cef845acd50
Gabriel Hakkinen <79407853+hakkGDC@users.noreply.github.com>
2022-04-20 15:08:53 -0300

    até aula 60
    Muitas alterações no commit porque tive que refazer a Persistence 2x por causa de problemas com a Migrations. Resolvido instalando o pacote Relational.

--- Back/src/ProEventos.API/Controllers/EventosController.cs
+++ Back/src/ProEventos.API/Controllers/EventosController.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
-using ProEventos.API.Data;
-using ProEventos.API.Models;
+using ProEventos.Domain;
+using ProEventos.Persistence;
 
 namespace ProEventos.API.Controllers
 {
@@ -14,8 +14,8 @@ namespace ProEventos.API.Controllers
     public class EventosController : ControllerBase
     {
        
-        private readonly DataContext _context;
-        public EventosController(DataContext context)
+        private readonly ProEventosContext _context;
+        public EventosController(ProEventosContext context)
         {
             _context = context;
         }
@@ -29,7 +29,7 @@ namespace ProEventos.API.Controllers
 
         public Evento GetById(int id)
         {
-            return _context.Eventos.FirstOrDefault(evento => evento.EventoId == id);
+            return _context.Eventos.FirstOrDefault(evento => evento.Id == id);
         }            
     
         [HttpPost]
